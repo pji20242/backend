fa75db57e1703d84982888437d0a40a7e8945716
arthurcadore <arthurbarcella.ifsc@gmail.com>
2024-11-05 00:28:16 +0000

    :sparkles: reconfigured the message test format

--- mqtt-broker/test/mqtt-test.sh
+++ mqtt-broker/test/mqtt-test.sh
@@ -2,11 +2,13 @@
 
 # Loop infinito para publicar mensagens a cada 1 segundo
 while true; do
-    # Gera um valor de temperatura aleatório entre 1 e 50
+    # Gera um valor de temperatura, pressao e luminosidade aleatório
     temperatura=$((RANDOM % 50 + 1))
+    pressao=$((RANDOM % 200 + 1))
+    luminosidade=$((RANDOM % 1500 + 1))
     
     # Publica o valor de temperatura usando o mosquitto_pub
-    mosquitto_pub -h 127.0.0.1 -p 1883 -t pji3 -m "temperatura $temperatura"
+    mosquitto_pub -h 127.0.0.1 -p 1883 -t pji3 -m "1f3cbe5b-15dd-483e-a74a-bea00227da11%T=$temperatura%P=$pressao%L=$luminosidade@"
     
     # Aguarda 1 segundo antes de repetir
     sleep 1
